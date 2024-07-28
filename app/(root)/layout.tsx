143593bd7a079d852822c82371144b15b3102f45
amin <aminqanbarii1992@gmail.com>
2024-07-29 03:22:13 +0330

    work on leftsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <p className="text-white-1">left sidebar</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar/>
         {children}
         <p className="text-white-1">right sidebar</p>
       </main>
